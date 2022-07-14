ff065b16341f2cdc5d6b4da03d90abffbe89ef5c
bravoxl <719321682@qq.com>
2022-07-14 16:42:50 +0800

    1

--- zxl_AI_FaceDetection.py
+++ zxl_AI_FaceDetection.py
@@ -1,17 +1,20 @@
 #Face decetion
 import cv2
+from random import randrange
 trained_face_data=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-img=cv2.imread("Zongze.jpg")
+img=cv2.imread("4.jpg")
 
 grayscaled_img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 face_coordinates=trained_face_data.detectMultiScale(grayscaled_img)
-(x,y,w,h)= face_coordinates[0]
+
+for (x,y,w,h) in face_coordinates:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (randrange(256), randrange(256), randrange(256)), 10)
 
 # print(face_coordinates)
-cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
 
-cv2.imshow("Face Detector",img)
+
+cv2.imshow("Face Detector", img)
 cv2.waitKey()
 
 print("zxl ! bravo!")
